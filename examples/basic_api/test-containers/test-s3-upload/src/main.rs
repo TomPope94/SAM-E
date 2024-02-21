bb1c3d85a656fe6624b293f3cb79054ef9ec2997
--get <tompope1994@gmail.com>
2024-02-21 14:49:34 +0000

    Working examples pre-s3 event handling

--- examples/basic_api/test-containers/test-s3-upload/src/main.rs
+++ examples/basic_api/test-containers/test-s3-upload/src/main.rs
@@ -1,12 +1,13 @@
-use aws_config::{profile::ProfileFileCredentialsProvider, BehaviorVersion};
-use aws_sdk_s3::{config::Region, Client};
+use aws_config::BehaviorVersion;
+use aws_sdk_s3::{Config, config::Region, Client};
 use aws_lambda_events::event::sqs::SqsEvent;
 
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
-use tracing::info;
+use tracing::{debug, info};
 use tracing_subscriber::EnvFilter;
 
+use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -16,28 +17,49 @@ async fn main() -> Result<(), Error> {
         .with_ansi(false)
         .without_time()
         .init();
-
     info!("Starting S3 Lambda...");
-    let region = Region::new("eu-west-2");
-
-    let profile_provider = ProfileFileCredentialsProvider::builder()
-        .profile_name("staging-mfa")
-        .build();
-
-    let config = aws_config::defaults(BehaviorVersion::v2023_11_09())
-        .region(region)
-        .credentials_provider(profile_provider)
-        .endpoint_url("http://s3-local:9000")
-        .load()
-        .await;
-
-    let _client = Client::new(&config);
 
     run(service_fn(function_handler)).await
 }
 
 async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
     info!("Received event: {:?}", event);
+    debug!("Processing {} records", event.payload.records.len());
+    debug!("Creating S3 client...");
+
+    let region = Region::new("eu-west-2");
+
+    let raw_config = Config::builder()
+        .behavior_version(BehaviorVersion::v2023_11_09())
+        .region(region)
+        .force_path_style(true)
+        .endpoint_url("http://s3-local:9000")
+        // .credentials_provider(ProfileFileCredentialsProvider::builder().profile_name("staging-mfa").build())
+        .build();
+
+    let client = Client::from_conf(raw_config);
+    debug!("Client created successfully");
+
+    let messages = event.payload.records;
+    for message in messages {
+        if let (Some(body), Some(id)) = (message.body, message.message_id) {
+            debug!("Writing file inside container: {}", id);
+            let file_name = format!("{}.txt", id);
+            let file_path = format!("/tmp/{}", file_name);
+            fs::write(&file_path, body.as_bytes()).expect("Unable to write file");
+
+            let file_bytes = fs::read(&file_path).expect("Unable to read file");
+
+            debug!("Uploading file to S3: {}", file_name);
+            let _response = client
+                .put_object()
+                .bucket("test-bucket-001")
+                .key(&file_name)
+                .body(file_bytes.into())
+                .send()
+                .await?;
+        }
+    }
 
     Ok(())
 }
