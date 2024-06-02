d0c0740872220149a87fc4f55ea1487e98f68447
Tom Pope <48588640+TomPope94@users.noreply.github.com>
2024-06-02 16:57:32 +0100

    Fixes issue with sqs messages from eventbridge (#31)

--- examples/eventbridge/test-sqs-handler/src/msg_handler.rs
+++ examples/eventbridge/test-sqs-handler/src/msg_handler.rs
@@ -1,10 +1,10 @@
 mod handlers;
 
-use aws_lambda_events::event::sqs::SqsEvent;
+use aws_lambda_events::event::sqs::SqsEventObj;
 use lambda_runtime::{LambdaEvent, Error};
 use tracing::{debug, info};
 
-pub async fn msg_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
+pub async fn msg_handler(event: LambdaEvent<SqsEventObj<serde_json::Value>>) -> Result<(), Error> {
     info!("Received event: {:?}", event);
 
     for record in event.payload.records {
