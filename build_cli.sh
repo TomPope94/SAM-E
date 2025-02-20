f39862115530961eb7a88f70ca5c5ce1c598f5c8
--get <tompope1994@gmail.com>
2025-02-20 23:04:00 +0000

    Fixes build.sh and docker compose

--- build_cli.sh
+++ build_cli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Build the CLI
-cd sam-e-cli && cargo zigbuild --release --target x86_64-unknown-linux-gnu && cargo zigbuild --release --target aarch64-unknown-linux-gnu;
+cd sam-e-cli && cargo build --release && sudo cp target/release/sam-e-cli /usr/local/bin/sam-e
 
-# Detect architecture and copy appropriate binary
-if [ "$(uname -m)" = "aarch64" ]; then
-    sudo cp target/aarch64-unknown-linux-gnu/release/sam-e-cli /usr/local/bin/sam-e
-else
-    sudo cp target/x86_64-unknown-linux-gnu/release/sam-e-cli /usr/local/bin/sam-e
-fi
+# # Detect architecture and copy appropriate binary
+# if [ "$(uname -m)" = "aarch64" ]; then
+#     sudo cp target/aarch64-unknown-linux-gnu/release/sam-e-cli /usr/local/bin/sam-e
+# else
+#     sudo cp target/x86_64-unknown-linux-gnu/release/sam-e-cli /usr/local/bin/sam-e
+# fi
